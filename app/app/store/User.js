824ff3847f62278ff46f700988467b127e37a7e3
Anibal Copitan <anibal.copitan@altimea.com>
2017-11-19 21:37:02 -0500

    [fix] Userstore no load twice times

--- app/app/store/User.js
+++ app/app/store/User.js
@@ -2,7 +2,7 @@ Ext.define('MyApp.store.User', {
 	extend: 'Ext.data.Store',
 	requires: ['MyApp.model.User'],
 	config: {
-		autoLoad: true,
+		autoLoad: false,
 		model: 'MyApp.model.User',
 		storeId: 'Users',
 		autoSync: true
